25f53b31bd8ae13e969bb6d4f7caa3b94c531a8b
Walsh, Ethan Joseph <ethan.walsh282003@gmail.com>
2024-09-19 20:30:36 -0400

    N332ShootingGallery2
    Cubes fall and now have a cube layer

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    //initialize list for cubes *NOT WORKING* :(
-    protected List<GameObject> cubesList;
+    //making 
+    [SerializeField]
+    private LayerMask cubeMask;
+
+    //initialize list for cubes
+    protected List<GameObject> cubesList = new List<GameObject>();
     //initialize cube prefab reference
-    public GameObject cubePrefab;
+    [SerializeField]
+    private GameObject cubePrefab;
 
     // Start is called before the first frame update
     private void Start()
     {
-
         //for loop to spawn cubes
         for (int i = 0; i < 10; i++)
         {
             //get random position on screen
-            //Camera.main.ViewportToWorldPoint(
-            Vector3 randPosInView = new Vector3(Random.Range(-10, 10), 0, Random.Range(-7, 7));
+            Vector3 randPosInView = new Vector3(Random.Range(-7, 6), 5, Random.Range(-4, 7));
             //instantiate cube at random position
             Instantiate(cubePrefab, randPosInView, Quaternion.identity);
 
-            //add that cube to list *NOT WORKING* :(
-            // cubesList.Add(cubePrefab);
+            //add that cube to list
+            cubesList.Add(cubePrefab);
         }
     }
 
@@ -31,7 +34,7 @@ public class GameController : MonoBehaviour
     private void Update()
     {
         //check for mouse click
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
         {
             //gets the position of the mouse
             Vector2 mousePos = Input.mousePosition;
@@ -40,25 +43,29 @@ public class GameController : MonoBehaviour
 
             //raycast at mouse click location
             //"out" will mark it as output rather than input
-            if (Physics.Raycast(ray, out rch))
+            if (Physics.Raycast(ray, out rch, 1000f, cubeMask))
             {
                 // the object we hit
                 GameObject objToRemove = rch.collider.gameObject;
 
-                //delete object clicked from list *NOT WORKING* :(
-                // cubesList.Remove(objToRemove);
-
-                //delete object clicked from game
-                Destroy(objToRemove);
-
-                //set random position variable
-                Vector3 randPosInView = new Vector3(Random.Range(-10, 10), 0, Random.Range(-7, 7));
-                //create new cube in view
-                Instantiate(cubePrefab, randPosInView, Quaternion.identity);
-
-                //add new cube to list *NOT WORKING* :(
-                // cubesList.Add(cubePrefab);
+                ResetCube(objToRemove);
             }
         }
+    }
+
+    private void ResetCube(GameObject cube)
+    {
+        //delete cube from list
+        cubesList.Remove(cube);
+
+        //delete cube from game
+        Destroy(cube);
+
+        //add new cube to game
+        Vector3 randPosInView = new Vector3(Random.Range(-10, 10), 0, Random.Range(-7, 7));
+        Instantiate(cubePrefab, randPosInView, Quaternion.identity);
+
+        //add new cube to list
+        cubesList.Add(cubePrefab);
     }
 }
