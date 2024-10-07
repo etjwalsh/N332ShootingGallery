8dae3136264e6cb4dedfe1a426476fb55cdafb48
Walsh, Ethan Joseph <ethan.walsh282003@gmail.com>
2024-10-06 20:01:53 -0400

    anchors placed

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,12 +8,12 @@ public class Score : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI Hits;
     [SerializeField] private TextMeshProUGUI Misses;
+    [SerializeField] private GameObject resetTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
-        Hits.text = "hi";
-        Misses.text = "bye";
+
     }
 
     // Update is called once per frame
