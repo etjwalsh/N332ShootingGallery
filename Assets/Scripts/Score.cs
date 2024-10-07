0f3963bfae78f718d006bd4199d82b8cb3667ba5
Walsh, Ethan Joseph <ethan.walsh282003@gmail.com>
2024-10-06 21:29:16 -0400

    main menu prefab added

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -9,16 +9,19 @@ public class Score : MonoBehaviour
     [SerializeField] private TextMeshProUGUI Hits;
     [SerializeField] private TextMeshProUGUI Misses;
     [SerializeField] private GameController resetTrigger;
+    [SerializeField] private GameObject crosshairs;
 
     // Update is called once per frame
     void Update()
     {
         Hits.text = resetTrigger.numHit.ToString();
         Misses.text = resetTrigger.numMissed.ToString();
+        crosshairs.transform.position = Input.mousePosition;
     }
 
     public void ExitClicked()
     {
+        Debug.Log("Exit clicked");
         UnityEditor.EditorApplication.isPlaying = false;
 
         //for actual build of game 
