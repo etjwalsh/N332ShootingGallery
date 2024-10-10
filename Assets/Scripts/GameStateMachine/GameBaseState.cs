b520fb3eef90db3b133c9e8e5b39c4b666e0b4d2
Walsh, Ethan Joseph <ethan.walsh282003@gmail.com>
2024-10-09 22:24:13 -0400

    Revert "set up state machine controller and states"
    This reverts commit a086a406db5843480aa7c4ec5f09f4b654d7e8f2.

--- Assets/Scripts/GameStateMachine/GameBaseState.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public abstract class GameBaseState
-{
-    public abstract void EnterState(GameStateManager gameManager);
-    public abstract void UpdateState(GameStateManager gameManager);
-}
